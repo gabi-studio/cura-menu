089bbb388bcac005925edf195f7d7a922017fe17
Gabi Catalan <gabistudio.dev@gmail.com>
2025-04-13 03:33:26 -0400

    update files to ignore

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const AdminUser = require('../models/adminUser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 exports.login = async (req, res) => {
   const { username, password } = req.body;
